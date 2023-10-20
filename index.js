6d0dc8228dd15d851584583f6e34de545796ccd7
Kavit Patel <kvpatel.er@gmail.com>
2023-10-20 11:42:50 +0530

    fixed node engine

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { taskRouter } from "./routes/task-route.js";
 config({ path: config.env });
 const app = express();
 
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookie_parser());
 app.use(express.urlencoded({ extended: true }));
